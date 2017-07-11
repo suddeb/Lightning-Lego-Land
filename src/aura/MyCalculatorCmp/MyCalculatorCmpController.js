6fdb9349e7a170f547898bf2a8534f28d850e61a
sudipta <sudipta.deb@gmail.com>
2017-07-11 00:41:02 -0400

    Added the controller logic

--- src/aura/MyCalculatorCmp/MyCalculatorCmpController.js
+++ src/aura/MyCalculatorCmp/MyCalculatorCmpController.js
@@ -1,5 +1,10 @@
 ({
-	myAction : function(component, event, helper) {
-		
+	doAdd : function(component, event, helper) {
+		var firstNumber = component.get("v.firstNumber");
+		var secondNumber = component.get("v.secondNumber");
+		console.log(firstNumber + ' ' + secondNumber);
+		var result = firstNumber + secondNumber;
+		console.log('Result: ' + result);
+		component.set("v.result", result);
 	}
 })
